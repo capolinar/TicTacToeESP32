562137a4261a9694302d0a358051646e838b4881
Cody Apolinar <cnapolinar@cpp.edu>
2022-12-14 20:11:58 -0800

    Outline for program

---
+++ TicTacToe.c
@@ -0,0 +1,14 @@
+//headers go here
+
+//setup for mqtt
+
+//setup for tic tac toe game
+
+//main
+
+//code for mqtt
+
+//copy and paste ticTacToe from class assignment
+
+//most likey add mqtt integration
+
