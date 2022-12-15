7586446c0488ab4fc51d06dde9da4ba20d3ef142
Cody Apolinar <cnapolinar@cpp.edu>
2022-12-14 20:17:49 -0800

    Added win function

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,6 +1,10 @@
 //headers go here
 #include "stdlib.h"
 #include "string.h"
+#include <stdio.h>
+#include  <stdlib.h> 
+#include  <time.h>
+#include <stdbool.h>
 #include "unistd.h"
 #include "MQTTClient.h"
 
@@ -52,6 +56,44 @@ void printBoard()
     printf("+-----------+\n");
     printf("| %c | %c | %c |\n", square[7], square[8], square[9]);
     printf("+-----------+\n");
+}
+
+int win() //function that returns 1 if game is over, -1 if in progress, and 0 if over and no result(TIE)
+{                                                             //each of these check if X or O are in a row
+                                                        //essentially checks all combinations of win conditions
+   
+    if (square[1] != ' ' && square[2] != ' ' && square[1] == square[2] && square[2] == square[3]) //top row
+        return 1;
+        
+    else if (square[4] != ' ' && square[5] != ' ' &&  square[4] == square[5] && square[5] == square[6])//middle row
+        return 1;
+        
+    else if (square[7] != ' ' && square[8] != ' ' && square[7] == square[8] && square[8] == square[9])//bottom row
+        return 1;
+        
+    else if (square[1] != ' ' && square[4] != ' ' && square[1] == square[4] && square[4] == square[7])//left column
+        return 1;
+        
+    else if (square[2] != ' ' && square[5] != ' ' && square[2] == square[5] && square[5] == square[8])//middle column
+        return 1;
+        
+    else if (square[3] != ' ' && square[6] != ' ' && square[3] == square[6] && square[6] == square[9])//bottom column
+        return 1;
+        
+    else if (square[1] != ' ' && square[5] != ' ' && square[1] == square[5] && square[5] == square[9])//left diaganol
+        return 1;
+        
+    else if (square[5] != ' ' && square[5] != ' ' && square[3] == square[5] && square[5] == square[7])//right diagnol
+        return 1;
+        
+    else if (square[1] != ' ' && square[2] != ' ' && square[3] != ' ' && //checks if any remaining spaces
+        square[4] != ' ' && square[5] != ' ' && square[6] != ' ' && square[7] 
+        != ' ' && square[8] != ' ' && square[9] != ' ')
+
+        return 0; //if no remaining spaces, game is over(tie)
+    else
+        return  - 1; //if there is remaining spaces, game is in progress
+
 }
 
 //main
