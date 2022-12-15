be3271306920e28b654c6ea328abe8e809c58d31
Cody Apolinar <cnapolinar@cpp.edu>
2022-12-14 20:14:41 -0800

    Added main from eqmx(broker) documentation

--- TicTacToe.c
+++ TicTacToe.c
@@ -38,6 +38,46 @@ int on_message(void *context, char *topicName, int topicLen, MQTTClient_message 
 
 //main
 
+int main(int argc, char *argv[]) {
+    int rc;
+    MQTTClient client;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID, 0, NULL);
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    conn_opts.username = USERNAME;
+    conn_opts.password = PASSWORD;
+    MQTTClient_setCallbacks(client, NULL, NULL, on_message, NULL);
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(-1);
+    } else {
+        printf("Connected to MQTT Broker!\n");
+    }
+    // subscribe topic
+    MQTTClient_subscribe(client, TOPIC, QOS);
+    char payload[16];
+	
+    for (int i = 0; i < 100; i += 1) {
+        // publish message to broker
+        //snprintf(payload, 16, "message-%d", i);
+        //publish(client, TOPIC, payload);
+		printf("\n Iteration %d\n",i);
+		char* x;
+		
+		scanf("%s", x);
+			
+		publish(client, TOPIC, x);
+		
+        sleep(1);
+    }
+
+
+	
+    MQTTClient_disconnect(client, TIMEOUT);
+    MQTTClient_destroy(&client);
+    return rc;
+}
+
 //code for mqtt
 
 //copy and paste ticTacToe from class assignment
